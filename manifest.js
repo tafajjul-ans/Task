0c927c6eee90fb3369a72ac70a1784a99d6df41a
Tafajjul Ansari <tafajjulans141@gmail.com>
2025-08-22 20:45:15 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+  "name": "Auto-Update Demo App",
+  "short_name": "Tasks",
+  "start_url": "/index.html",
+  "display": "standalone",
+  "background_color": "#f2f2f2",
+  "theme_color": "#141308",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
