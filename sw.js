0ecf1b602d9f1e50ae9d8dffa7d46a7d71f3a23e
Tafajjul Ansari <tafajjulans141@gmail.com>
2025-08-23 11:09:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "demo-cache-v1";
+const CACHE_NAME = "task-cache-v2"; // version badalna hamesha new update me
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -8,33 +8,24 @@ const FILES_TO_CACHE = [
   "/icon-512.png"
 ];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
-  self.skipWaiting();
+  self.skipWaiting(); // force install
 });
 
-// Activate
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
-  self.clients.claim();
+  self.clients.claim(); // force control
 });
 
-// Fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
 });
-self.addEventListener('message', (event) => {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
-});
-
